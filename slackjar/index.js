cbe824c2794e4ffbb0b0f743fd007773ed65ccf6
Mo-Sho <muhama94@gmail.com>
2018-01-14 11:17:30 -0800

    ready to deploy beta

---
+++ slackjar/index.js
@@ -0,0 +1,49 @@
+const phone = document.getElementById('convo');
+const paths = [
+  "images/PropyMessage.png",
+  "images/PropySend.png"
+];
+const messages = [];
+// for (var i = 0; i < paths.length; i++) {
+//   messages[i] = new Image();
+//   messages[i].src = paths[i];
+//
+// }
+
+// const messages = [
+//   "Congratulations guys!, you have won the hackathon!!!",
+//   "WE DID IT BABY!!!",
+//   "You have received 1,000,000 Propy coins!!!"
+// ]
+// const addMessage = (message) => {
+//   const p = document.createElement("P");
+//   const text = document.createTextNode(message);
+//   p.appendChild(text);
+//   phone.appendChild(p);
+// }
+// let i = 0;
+// setTimeout(() => {
+//   addMessage(messages[i]);
+//
+//     addMessage(messages[i])
+// }
+// , 3000)
+
+// const x = function() {
+//     let counter = 0;
+//
+//     (function foo() {
+//        const div = document.createElement("DIV");
+//        const path = paths[counter];
+//        div.style.backgroundImage = `url(${path})` ;
+//        div.style.height = "100px";
+//        div.style.width = "90%";
+//        phone.appendChild(div);
+//
+//         if (counter < 1) {
+//             setTimeout(foo, 3000);
+//             counter++;
+//         }
+//     })();
+// };
+// x();
